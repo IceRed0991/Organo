46f3a11405515f65740bc5d8c8152e1ea6b83ee3
IceRed0991 <wernergutschow@hotmail.com>
2023-06-13 17:55:35 -0300

    Criando formulário

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,11 @@
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
 import "./Formulario.css";
 
 const Formulario = () => {
+  
+  const times=['Programação','Front-end', 'Data Science', 'Devops', 'Ux e Desing', 'Mobile', 'Inovação e Gestão'];
+
   return (
     <section className="formulario">
       <form>
@@ -9,6 +13,7 @@ const Formulario = () => {
         <CampoTexto label="Nome" placeholder="Digite seu nome" />
         <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
         <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa label="Time" itens={times}/>
       </form>
     </section>
   );
