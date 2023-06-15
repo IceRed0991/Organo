5fc9cc49e104d38b28e0b02eefeffc9ce5c45cac
IceRed0991 <wernergutschow@hotmail.com>
2023-06-15 17:58:52 -0300

    Criando Lista Suspensa

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,11 +1,17 @@
 import './CampoTexto.css'
-const CampoTexto =(props)=>{
-return(
-    <div className="campo-texto">
-        <label>{props.label}</label>
-        <input placeholder={props.placeholder}/>
-    </div>
-)
+const CampoTexto = (props) => {
+
+    let valor = '';
+    const aoDigitado=(evento)=>{
+        valor=evento.target.value;
+        console.log(valor)
+    }
+    return (
+        <div className="campo-texto">
+            <label>{props.label}</label>
+            <input onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder} />
+        </div>
+    )
 
 }
 export default CampoTexto
