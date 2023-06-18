fc301d55acc09f626944dec37559e3df6f09a4f5
IceRed0991 <wernergutschow@hotmail.com>
2023-06-18 11:59:39 -0300

    Refatorando formulario e times

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,8 +1,12 @@
 import './Time.css'
 
-const Time=()=>{
-    return(
+const Time=(props)=>{
+    const css = { backgroundColor: props.corSecundaria }
 
+    return(
+            <section className='time' style={css}>
+            <h3 style={{borderColor:props.corPrimaria}} >{props.nome}</h3>
+        </section>
     )
 }
 export default Time
